823f94c52f3826044d0b800429b193c0fb4e126d
Jhoanna Rosa Mera Dávila <jhoannarosameradavila@gmail.com>
2023-02-16 18:35:08 -0500

    formulario para agregar una nota y contenedor de notas

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -5,15 +5,24 @@ import { auth } from '../init';
 import { logOut } from '../lib/firabase/methodsAuth';
 
 
-
-
-
 export default function NotesPage() {
-    return (<div>RemindMe
+    return (
+    <div>
+        <div className='container-header'>
+            RemindMe
+            <h2>Notes</h2>
+            {/* <button className='btn-logout' onClick={logOut(auth)}> Logout</button> */}
+            <Link to="/" onClick={logOut(auth)}>Logout</Link>
+        </div>
 
-        <h2>Notes</h2>
-        {/* <button className='btn-logout' onClick={logOut(auth)}> Logout</button> */}
-        <Link to="/" onClick={logOut(auth)}>Salir</Link>
+        <form className='form-note'>
+            <textarea className='text-note'></textarea>
+            <button className='save-btn'>save</button>
+        </form>
 
-    </div>);
+        <div className='container-notes'>
+
+        </div>
+    </div>
+    );
 }
